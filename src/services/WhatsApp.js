1c686c1391d2a17ae12d187060241789f32fef0d
bozdoz <howaboutben@gmail.com>
2020-07-24 12:32:29 -0300

    whatsapp with terminal qr-code

--- src/services/WhatsApp.js
+++ src/services/WhatsApp.js
@@ -1,44 +1,21 @@
 const {
   errors: { TimeoutError },
-} = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
+} = require('puppeteer-core');
+const qrcode = require('qrcode-terminal');
 const Service = require('./Service');
 const { getConfig } = require('../config');
-const logger = require('../logger');
-const { saveCookies } = require('../cookies');
-const { wait } = require('../util');
-
-const log = logger('WhatsApp');
 
 class WhatsApp extends Service {
   async navigateToURL() {
     const { headless } = getConfig();
     const url = 'https://web.whatsapp.com';
 
-    log(`Navigating to ${url}`);
+    const page = await this.getPage(url);
+    // selector which indicates successful loading
+    const appSelector = '.selectable-text';
 
-    // whatsapp needs the user agent in order to show the QR code login
-    // first, add listeners to get and update user agent
-    const settingUserAgent = this.setUserAgent();
-    // second, send a request to get user agent
-    await this.page.goto(url);
-    log('navigated');
-
-    // last, make sure the user agent is updated
-    await Promise.race([settingUserAgent, wait(5000)]);
-
-    log('agent set');
-
-    const errorMessage = () =>
-      // eslint-disable-next-line no-console
-      console.error(
-        'TimeoutError: You may need to scan the QR code with your phone'
-      );
-
-    // wait for app, if headless, summon a window
     try {
-      await this.page.waitForSelector('.app', {
+      await page.waitForSelector(appSelector, {
         timeout: 2000,
       });
     } catch (e) {
@@ -53,21 +30,19 @@ class WhatsApp extends Service {
 
     try {
       await this.screenshot('before-app.png');
-      await this.page.waitForSelector('.app');
+      await page.waitForSelector(appSelector);
+      // kills the looping QR code script
+      this.scanned = true;
       await this.screenshot('after-app.png');
     } catch (e) {
       await this.screenshot('app-timeout.png');
       if (e instanceof TimeoutError) {
-        errorMessage();
+        // eslint-disable-next-line no-console
+        console.error(
+          'TimeoutError: You may need to scan the QR code with your phone'
+        );
       }
       throw e;
-    }
-
-    try {
-      // once logged in, save cookies
-      await saveCookies(this.page);
-    } catch (e) {
-      log('failed saving cookies');
     }
   }
 
@@ -89,71 +64,32 @@ class WhatsApp extends Service {
     await clickOnText('Type a message');
   }
 
-  /**
-   * @returns {Promise<void>}
-   */
-  async setUserAgent() {
-    const { headless } = getConfig();
-    if (!headless) {
-      return Promise.resolve();
+  async getQRCode() {
+    const canvasSelector = 'canvas';
+
+    try {
+      await this.page.waitForSelector(canvasSelector);
+    } catch (e) {
+      // if the canvas isn't there, there's no QR code
+      return;
     }
 
-    await this.page.setRequestInterception(true);
-    let userAgentResolve;
-    const userAgentPromise = new Promise((resolve) => {
-      userAgentResolve = resolve;
-    });
-    let userAgent;
+    const imageData = await this.page.evaluate(
+      (selector) =>
+        document.querySelector(selector).parentElement.getAttribute('data-ref'),
+      canvasSelector
+    );
 
-    this.page.on('request', (request) => {
-      const headers = request.headers();
+    if (imageData !== this.imageData) {
+      qrcode.generate(imageData, { small: true });
+      this.imageData = imageData;
+    }
 
-      log('request url', request.url());
-      log('request headers', headers);
-
-      if (!userAgent) {
-        try {
-          userAgent = headers['user-agent'];
-
-          log('Found userAgent', userAgent);
-
-          // remove "Headless" is the key
-          userAgent = userAgent.replace('Headless', '');
-          // update current request
-          headers['user-agent'] = userAgent;
-
-          // update future
-          this.page.setUserAgent(userAgent).then(userAgentResolve);
-        } catch (e) {
-          // pass
-        }
+    setTimeout(() => {
+      if (!this.scanned) {
+        this.getQRCode();
       }
-
-      request.continue({ headers });
-    });
-
-    return userAgentPromise;
-  }
-
-  async getQRCode() {
-    await this.page.waitForSelector('canvas');
-
-    // screenshot QR code to scan
-    await this.screenshot('qr.png');
-  }
-
-  async screenshot(filename) {
-    const img = path.join('screenshots', filename);
-
-    fs.mkdirSync('screenshots', {
-      recursive: true,
-    });
-
-    await this.page.screenshot({
-      path: img,
-    });
-
-    log('screenshotted: ', img);
+    }, 2000);
   }
 }
 
